33043bf622853e93f469ac346ff1e055916be933
Karthik T <178karthik@gmail.com>
2026-06-25 17:10:20 +0530

    Revert "improvised APIClient"
    This reverts commit be89764218fba4c00f9e62c0c3ef7c7275e79b28.

--- tests/api/petStore.spec.ts
+++ tests/api/petStore.spec.ts
@@ -1,24 +1,22 @@
-import { test, expect } from '../../fixtures'
-import { APIClient } from '../../api/APIClient'
+import {test,expect} from '../../fixtures'
+import  {APIClient}  from '../../api/APIClient'
 import { APIResponse } from 'playwright';
 
-test.describe('PetStore Api Tests', () => {
+test.describe('PetStore Api Tests',()=>{
 
-    let firstId: string;
-
-    test('Finds Pet By Status', async ({ request }) => {
-
+    test('Finds Pet By Status',async({request})=>{
+        
         const api = new APIClient(request);
-        let response: APIResponse = await api.get('v2/pet/findByStatus?status=sold');
+        let response :APIResponse = await api.get('v2/pet/findByStatus?status=sold');
         const pets = await response.json();
-        firstId = pets[0].id;
+        const firstId = pets[0].id;
         console.log(firstId);
-        let allIds = pets.map(pet => pet.id);
-        console.log(allIds)
+        let allIds = pets.map(pet=>pet.id);
+        console.log(allIds) 
     })
-    test('Add a new pet to the store', async ({ request }) => {
+     test('Add a new pet to the store',async({request})=>{
         const api = new APIClient(request);
-
+    
         const payload: Record<string, unknown> = {
             "id": 0,
             "category": {
@@ -36,20 +34,9 @@ test.describe('PetStore Api Tests', () => {
             "status": "available"
         };
 
-        let response: APIResponse = await api.post('v2/pet', payload);
-        expect(await response.status()).toBe(200)
+        let response :APIResponse = await api.post('v2/pet', payload);
         const postResponse = await response.json();
         expect(postResponse.status).toBe("available")
-
+       
     })
-    test('Deletes a pet', async ({ request }) => {
-        const api = new APIClient(request);
-        const endpoint = 'v2/pet/' + firstId;
-        const deleteResponse = await api.delete(endpoint)
-        expect(await deleteResponse.status()).toBe(200)
-        console.log(await deleteResponse.json())
-
-
-    })
-
 })
